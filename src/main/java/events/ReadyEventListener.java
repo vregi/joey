bc1bdc04dec057b397e3c89b7acde67b89e25e1f
Vladyslav <vreginalld@gmail.com>
2023-11-20 02:14:42 +0300

    pom update

--- src/main/java/events/ReadyEventListener.java
+++ src/main/java/events/ReadyEventListener.java
@@ -3,11 +3,12 @@ package events;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
+import org.jetbrains.annotations.NotNull;
 
 public class ReadyEventListener implements EventListener {
 
     @Override
-    public void onEvent(GenericEvent event){
+    public void onEvent(@NotNull GenericEvent event){
         if(event instanceof ReadyEvent){
             System.out.println("sudo$ ready...");
         }
